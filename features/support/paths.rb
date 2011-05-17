b82e9751abf6e968635c56045c4475fb66f1a5df
Raoul Felix <rf@rfelix.com>
2011-05-17 15:12:44 +0100

    Added title functionality for show page

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,13 +10,8 @@ module NavigationHelpers
 
     when /the home\s?page/
       '/'
-
-    # Add more mappings here.
-    # Here is an example that pulls values out of the Regexp:
-    #
-    #   when /^(.*)'s profile page$/i
-    #     user_profile_path(User.find_by_login($1))
-
+    when /the project page for "([^\"]*)"/
+      project_path(Project.find_by_name!($1))
     else
       begin
         page_name =~ /the (.*) page/
