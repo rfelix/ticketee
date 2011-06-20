b2ed0e3c08824b87199b84a309680c1560e022b1
Raoul Felix <rf@rfelix.com>
2011-06-20 22:39:55 +0100

    Users should be redirect back to the projects page if they try going to a project that doesn't exist.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_filter :find_project, :only => [:show, :edit, :update, :destory]
+
   def index
     @projects = Project.all
   end
@@ -8,7 +10,6 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Project.new(params[:project])
     if @project.save
       flash[:notice] = "Project has been created."
       redirect_to @project
@@ -19,11 +20,9 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
@@ -39,9 +38,17 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
     flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
+  end
+
+  private
+
+  def find_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found."
     redirect_to projects_path
   end
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,4 +2,10 @@ require 'spec_helper'
 
 describe ProjectsController do
 
+  it "displays an error messag when asked for a missing project" do
+    get :show, :id => "not-here"
+    response.should redirect_to(projects_path)
+    flash[:alert].should eql("The project you were looking for could not be found.")
+  end
+
 end
