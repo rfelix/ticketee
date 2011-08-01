cb1b5389b941dbb3b35792894fd850c222f30707
Raoul Felix <rf@rfelix.com>
2011-08-01 09:12:51 +0100

    Fixed bug in user_steps where unconfirmed attribute was a string and always evaluated to true

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,6 +1,6 @@
 Given /^there are the following users:$/ do |table|
   table.hashes.each do |attributes|
-    unconfirmed = attributes.delete("unconfirmed")
+    unconfirmed = (attributes.delete("unconfirmed") == "true")
     @user = User.create!(
       attributes.merge!(
         :password_confirmation => attributes[:password]
